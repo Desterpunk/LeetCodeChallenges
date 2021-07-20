f606db166d56dd6d9682e4f061cc4e6bfecfa2e8
Jhon Serna <jhonalfaber@hotmail.com>
2021-07-19 19:39:36 -0500

    3 Longest Substring Without Repeating Characters

---
+++ 3_LongestSubstringWithoutRepeatingCharacterss/src/main/java/com/mycompany/_longestsubstringwithoutrepeatingcharacters/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany._longestsubstringwithoutrepeatingcharacters;
+
+/**
+ *
+ * @author Pc-pro
+ */
+public class Main {
+    public static void main (String args[]) {
+        Solution s1 = new Solution();
+        int resultado = s1.lengthOfLongestSubstring("pwwkew");
+        System.out.println(resultado);
+    }
+}
---
+++ 3_LongestSubstringWithoutRepeatingCharacterss/src/main/java/com/mycompany/_longestsubstringwithoutrepeatingcharacters/Solution.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany._longestsubstringwithoutrepeatingcharacters;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Pc-pro
+ */
+public class Solution {
+   public int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max=0;
+        for (int i=0, j=0; i<s.length(); ++i){
+            if (map.containsKey(s.charAt(i))){
+                j = Math.max(j,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i-j+1);
+        }
+        return max;
+    }
+}
